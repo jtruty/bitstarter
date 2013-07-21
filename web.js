5766621a6d2ecdae5ce90acc0cdd608cba97d74d
jtruty <jtruty@gmail.com>
2013-07-21 23:58:25 +0000

    updating site and adding images

--- web.js
+++ web.js
@@ -1,13 +1,31 @@
-var express = require('express');
-var fs = require('fs');
-var app = express.createServer(express.logger());
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
 
-app.get('/', function(request, response) {
-  var index = fs.readFileSync("index.html");
-  response.send(index.toString());   
-});
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            console.log("not exists: " + filename);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write('404 Not Found\n');
+            res.end();
+            return;
+        }
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        res.writeHead(200, {'Content-Type':mimeType});
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+
+    }); //end path.exists
+}).listen(8080);
